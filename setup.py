aa588dd2f8ddea159bac763c2772713da5cf0915
sundara26071978 <sunjsg@gmail.com>
2024-03-25 17:52:09 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Sunj',
+    author_email= 'sunjupskilling@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
